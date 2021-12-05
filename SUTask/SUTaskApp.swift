21a1e9f8a3d2b1738d2076d926fe7508d08bcb9b
Anna <annaersh1994@gmail.com>
2021-12-05 15:44:23 +0300

    Initial Commit

---
+++ SUTask/SUTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUTaskApp.swift
+//  SUTask
+//
+//  Created by Anna Ershova on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SUTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
